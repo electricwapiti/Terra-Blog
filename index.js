e25a1ca1e1024ed6c3e5cce857d62ca27a33aac5
Jack Conneely <jack@conneely.org>
2025-07-31 12:59:26 -0500

    styling

---
+++ index.js
@@ -0,0 +1,11 @@
+import express from 'express';
+const app = express();
+const port = 3002;
+app.use(express.static('public'));
+app.get('/', (req, res) => {
+    res.render('index.ejs');
+});
+
+app.listen(port, () => {
+    console.log(`Server running on port ${port}`);
+});
